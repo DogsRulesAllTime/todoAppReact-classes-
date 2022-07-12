fcbff35df7ee3f540f82f858393e2b563d3fc746
PierDun <kuznetsoffar@ya.ru>
2022-07-12 22:47:17 +0300

    add structure for components.

--- src/index.js
+++ src/index.js
@@ -1,28 +1,14 @@
 import React from 'react';
 // import ReactDOM  from 'react-dom/client'; // react 18v 
-import ReactDOM  from 'react-dom'; // react 16v
-import AppHeader from './components/app-header';
-import SearcPanel from './components/search-panel';
-import TodoList from './components/todo-list';
+import ReactDOM from 'react-dom'
+import App from './components/app'
 //test git branches 
-const App = () => {
-  const todoData = [
-    {label:'Drink coffee', important: true, key: 1},
-    {label:'Create React App', important: false, key: 2},
-    {label:'Fix some life problems', important: true, key: 3}
-  ]
 
-  return (
-  <div>
-   <AppHeader />
-    <SearcPanel />
-    <TodoList todoData={todoData}/>
-  </div>
-  )
-}
+
 
 // render in react 18v
 // const container = document.getElementById('root')
 // ReactDOM.createRoot(container).render(el);
 // render in react 16v 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App />,
+ document.getElementById('root'))
